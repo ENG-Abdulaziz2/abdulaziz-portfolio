642092de9e019de88a1771a747083c4dffdfb123
ENG-Abdulaziz2 <azezalabdan@gmail.com>
2026-04-27 21:32:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const burger = document.getElementById("burger");
-const nav = document.querySelector(".nav-links");
+const burger = document.getElementById('burger');
+const nav = document.querySelector('.nav-links');
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("active");
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+ 
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
 });
