580a931d4a5b09cb66a45cf608e2f6a9fb28badb
ENG-Abdulaziz2 <azezalabdan@gmail.com>
2026-04-27 21:02:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
+const burger = document.getElementById("burger");
+const nav = document.querySelector(".nav-links");
 
-menuBtn.onclick = () => {
-    menu.classList.toggle("show");
-};
-
-const links = document.querySelectorAll(".nav-link");
-const sections = document.querySelectorAll("section");
-
-window.addEventListener("scroll", () => {
-    let current = "";
-
-    sections.forEach(section => {
-        const top = section.offsetTop - 80;
-        const height = section.clientHeight;
-
-        if (scrollY >= top && scrollY < top + height) {
-            current = section.id;
-        }
-    });
-
-    links.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-        }
-    });
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
