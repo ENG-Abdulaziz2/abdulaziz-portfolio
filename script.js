25821c681d5b5b22e1ab1606121f92f5f9f4909a
ENG-Abdulaziz2 <azezalabdan@gmail.com>
2026-05-04 11:10:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const burger = document.getElementById('burger');
-const nav = document.querySelector('.nav-links');
+var menuBtn = document.getElementById('Menu');
+var navLinks = document.getElementById('nav-links');
 
+menuBtn.onclick = function () {
+    navLinks.classList.toggle('show');
+}
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
-
- 
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        nav.classList.remove('active');
-    });
+var links = document.querySelectorAll('#nav-links a');
+links.forEach(function (link) {
+    link.onclick = function () {
+        navLinks.classList.remove('show');
+    }
 });
