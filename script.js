08692cb62006fa25585b8e8220905c49fc39f73b
ENG-Abdulaziz2 <azezalabdan@gmail.com>
2026-05-04 11:12:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var navLinks = document.getElementById('nav-links');
 
 menuBtn.onclick = function () {
     navLinks.classList.toggle('show');
-}
+};
 
 var links = document.querySelectorAll('#nav-links a');
+
 links.forEach(function (link) {
     link.onclick = function () {
         navLinks.classList.remove('show');
-    }
+    };
 });
